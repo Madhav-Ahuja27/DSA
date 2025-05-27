d3db8a0648d9d2f678bb5d81de19bcecba2aaaf4
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2025-05-27 21:52:33 +0530

    Create recursionPOW.java

---
+++ recursionPOW.java
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;  // Use long long to avoid overflow with INT_MIN
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return power(x, N);
+    }
+
+private:
+    double power(double x, long long n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+};
