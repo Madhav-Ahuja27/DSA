656f102a855749e9df8b32c747cbf4c43254c777
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2025-06-08 23:53:48 +0530

    Create containsDupes.java

---
+++ containsDupes.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i:nums){
+            hs.add(i);
+        }
+        return !(nums.length==hs.size());
+    }
+}
