1fbc43552d5ce286a49e5f0c36031705f3c9754c
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2025-07-08 23:58:28 +0530

    Create kokoEatingBananas.java

---
+++ kokoEatingBananas.java
@@ -0,0 +1,36 @@
+class Solution {
+    boolean check(int[] piles, int k, int h){
+        int count = 0;
+        for(int i=0;i<piles.length;i++){
+            if(piles[i] <= k){
+                count++;
+            }
+            else{
+                   if (piles[i] % k == 0) {
+                        count += piles[i] / k;
+                    } else {
+                        count += (piles[i] / k) + 1;
+                    }
+            }
+        }
+        if(count <= h){return true;}
+        return false;
+    }
+    int bs(int[] piles, int low, int high,int h){
+        if(low>high){return low;}
+
+        int mid = low + (high-low)/2;
+        if(check(piles,mid,h)==true){
+            return bs(piles,low,mid-1,h);
+        }else{
+            return bs(piles,mid+1,high,h);
+        }
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int m = piles[0];
+        for(int i:piles){
+            m = i>m ? i:m;
+        }
+        return bs(piles,1,m,h);
+    }
+}
